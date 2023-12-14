87745ef90bdfebda94ef6ded359548c0fe3b6f36
ahmed27037 <ahmedamjedosman@gmail.com>
2023-12-14 17:50:26 -0600

    navbar edit

--- components/main/NavBar.tsx
+++ components/main/NavBar.tsx
@@ -5,17 +5,18 @@ import React from 'react'
 import { UserCircleIcon } from '@heroicons/react/24/solid'
 
 const NavBar = () => {
+  
   return (
   <div className="w-full h-fit fixed top-0 shadow-lg shadow-orange-800/50 bg-orange-900/50 backdrop-blur-md z-50 px-10">
-    <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px] flex-wrap">
+    <div className="w-full h-full flex   flex-col space-y-10    items-center m-auto px-[10px]">
       <a
         href="#about-me"
         className="h-auto w-auto flex flex-row items-center"
         >
           <a href='/'>
           <UserCircleIcon
-            width={60}
-            height={60}
+            width={50}
+            height={50}
             className="cursor-pointer hover:animate-slowspin rounded-full hover:bg-slate-900 transition-colors"
           />
           </a> 
@@ -24,8 +25,8 @@ const NavBar = () => {
             Full Stack Development
           </span>
       </a>
-     <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-      <div className="flex items-center justify-between w-full h-auto border border-[#ff6a50] bg-[#060302] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+
+      <div className="flex flex-col items-center justify-between gap-14 border border-[#ff6a50] bg-[#060302]  px-[20px] py-[10px] rounded-full text-gray-200">
       <a href="#about-me" className="cursor-pointer">
           About me
         </a>
@@ -36,17 +37,17 @@ const NavBar = () => {
           Projects
         </a>
       </div>
-    </div>
 
-    <div className="flex flex-row gap-5">
+
+    <div className="flex flex-col justify-around gap-5 my-3">
   {Socials.map((social) => (
-    <div key={social.name} className="flex flex-col items-center">
+    <div key={social.name} className="flex items-center">
       <a className='z-30' href={social.href}>
         <Image
           src={social.src}
           alt={social.name}
-          width={27}
-          height={26}
+          width={38}
+          height={38}
           className='m-auto hover:opacity-10'
         />
       </a>
